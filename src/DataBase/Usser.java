5c9387f892638307aee09e485b4d9b0f9e237c42
Felipe <feiandres57@gmail.com>
2025-02-01 21:54:21 -0300

    Interfaz de registro completada, hay que programar la base de datos

--- src/DataBase/Usser.java
+++ src/DataBase/Usser.java
@@ -4,4 +4,11 @@ import java.io.Serializable;
 
 public class Usser implements Serializable {
 
+    private String Nombre, Contrasenia;
+
+    public Usser(String Nombre, String Contrasenia){
+        this.Nombre = Nombre;
+        this.Contrasenia = Contrasenia;
+    }
+
 }
--- src/Login.java
+++ src/Login.java
@@ -1,11 +1,17 @@
+import DataBase.Usser;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
 
 public class Login extends JFrame implements ActionListener {
 
+    public static void main(String[] args) {
+        new Login();
+    }
+
+    Point Ubicacion;
     JLabel Bienvenida, Email, Password;
     JTextField TFEmail, TFPassword;
     JButton Entrar, Registrase;
@@ -27,6 +33,8 @@ public class Login extends JFrame implements ActionListener {
     }
 
     private void Init(){
+
+        Ubicacion = this.getLocation();
 
         Bienvenida = new JLabel("Bienvenido");
         Bienvenida.setBounds(75,50,200,55);
@@ -69,17 +77,89 @@ public class Login extends JFrame implements ActionListener {
             new Main();
             this.setVisible(false);
         }
+        if(e.getSource() == Registrase){
+            new Register();
+        }
 
-    }
-    public static void main(String[] args) {
-        new Login();
     }
 
     public void ValidarDatos(String Usser, String Password){
 
     }
 
-    
+    public void AddUser(){
 
+    }
+
+    private class Register extends JFrame implements ActionListener{
+
+        JTextArea Mensaje;
+        JLabel NombreReg, ContraseniaReg;
+        JTextField NameReg, PassWordReg;
+        JButton AceptarReg, CancelarReg;
+
+        public Register(){
+            setLayout(null);
+            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+            this.setContentPane(new ImagenDeFondo(this));
+            setLocationRelativeTo(null);
+            setBounds((int) Ubicacion.getX()+25, (int) Ubicacion.getY()+15,300,450);
+
+            setVisible(true);
+
+            InitReg();
+        }
+
+        private void InitReg(){
+
+            NombreReg = new JLabel("Nombre de usuario");
+            NombreReg.setBounds(50,50,175,30);
+            add(NombreReg);
+
+            NameReg = new JTextField();
+            NameReg.setBounds(50,85,175,30);
+            add(NameReg);
+
+            ContraseniaReg = new JLabel("Contraseña");
+            ContraseniaReg.setBounds(50,140,175,30);
+            add(ContraseniaReg);
+
+            PassWordReg = new JTextField();
+            PassWordReg.setBounds(50,170,175,30);
+            add(PassWordReg);
+
+           AceptarReg = new JButton("Aceptar");
+           AceptarReg.setBounds(50,270,80,30);
+           AceptarReg.addActionListener(this);
+           add(AceptarReg);
+
+           CancelarReg = new JButton("Salir");
+           CancelarReg.setBounds(145,270,80,30);
+           CancelarReg.addActionListener(this);
+           add(CancelarReg);
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if(e.getSource() == CancelarReg) dispose();
+
+            if(e.getSource() == AceptarReg){
+
+                Usser NuevoUsuario = new Usser(NameReg.getText(),PassWordReg.getText());
+
+                /*try (FileOutputStream fileOut = new FileOutputStream("DataBase/Usuarios.bin");
+                     ObjectOutputStream objOut = new ObjectOutputStream(fileOut)) {
+                    objOut.writeObject(NuevoUsuario);
+                    System.out.println("Objeto guardado en persona.bin");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }*/
+
+            }
+
+        }
+    }
 
 }
